58d4e3fe1958ad9619d2f1bb2f65438a068fb762
tdfriday <tdfriday@gmail.com>
2014-01-25 15:01:26 -0500

    Working JS of hot and cold game

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-var count = 5;
-var answer = Math.floor(Math.random()*101);
-// this is to check the random number for testing purposes
-console.log(answer);
+var randNum = Math.floor(Math.random()*101);
+// print random number to console for testing
+console.log(randNum);
+var count = 0;
 var guess;
-while (count !== 0){
+var numDiff;
+while (randNum != guess){
+ count++;
  guess = prompt("Pick a number between 1 and 100");
+ var numDiff = Math.abs(randNum - guess);
 if (guess < 1 || guess > 100 || isNaN(guess)){
     alert("You must pick a real number that is more than 0 and less than 101");
 }
-else if (guess == answer){
-    alert("Congratulations! you guessed it!");
-    break;
+else if (numDiff === 0){
+    alert("Congratulations! you guessed it in " + count + " attempt(s)");
 }
-else{
-   count--;
-   alert("Number of tries left: " + count);
-   
+else if (numDiff < 6){ 
+   alert("You are hot!");  
 }
+else if (numDiff < 11 && numDiff > 5){
+    alert("You are warm");
+}
+else if (numDiff < 21 && numDiff > 10){
+    alert("You are cold");
+}
+else if(numDiff > 20){
+    alert("You are very cold");
+}
+else
+    alert("Are you sure you know how to play this game?");
 }
